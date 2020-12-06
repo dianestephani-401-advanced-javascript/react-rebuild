77048e52ffd8ab5ad1f3903d8d3a2100ada13eaa
dianestephani <diane.stephani@gmail.com>
2020-12-06 15:18:59 -0800

    the stuff changes colors

--- react-rebuild/src/components/to-do/list.js
+++ react-rebuild/src/components/to-do/list.js
@@ -1,19 +1,51 @@
 import React from 'react';
 import Toast from 'react-bootstrap/Toast';
 import Badge from 'react-bootstrap/Badge';
+import {PaginatedList} from 'react-paginated-list';
+//npm i react-paginated-list and npm i styled-components? Not sure about this ask joe
+import {If, Else, Then} from 'react-if';
 
-function List(){
+
+function List(props){
+//We have item list in props!!!!!! Dummy!
+
+  
   return(
-    <Toast>
-  <Toast.Header>
-  <Badge pill variant="success">
-    Success
-  </Badge>{' '}
-    <strong className="mr-auto">Bootstrap</strong>
-    <small>11 mins ago</small>
-  </Toast.Header>
-  <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-</Toast>
+    <PaginatedList
+      list = {props.itemList}
+      itemsPerPage = {5}
+      renderList = {list => (
+        <>
+        {list.map((item) => (
+          <Toast key={item._id}>
+            <Toast.Header>
+              
+              <If condition={!item.complete}>
+
+              <Then>
+              <Badge pill variant="danger">
+                Pending
+              </Badge>{' '}
+              </Then>
+
+              <Else>
+              <Badge pill variant="success">
+                Complete
+              </Badge>{' '}
+              </Else>
+
+              </If>
+
+              <strong className="mr-auto">{item.assignee}</strong>
+              <small>Difficulty: {item.difficulty}</small>
+            </Toast.Header>
+            <Toast.Body>{item.text}</Toast.Body>
+          </Toast>
+
+        ))}
+        </>
+      )}
+    />
   )
 }
 
